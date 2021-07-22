a60be399f71fd21207c78015c700e0bb289a37a3
Bryan <bryan.zamora@goitsa.me>
2021-07-21 23:41:21 -0500

    Se maqueta la pantalla de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private _router:Router,) { }
 
   ngOnInit(): void {
   }
+
+  homeRouter(){
+    this._router.navigate(['../home']);
+  }
+
+  typeInputF="password";
+  showPF:boolean=true;
+   mostrarPassword(){
+     if(this.typeInputF=="text"){
+       this.showPF=true;
+       this.typeInputF="password";
+     }else{
+       this.showPF=false;
+       this.typeInputF="text";
+     }
+   }
 
 }
